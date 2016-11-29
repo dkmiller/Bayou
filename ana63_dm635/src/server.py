5df125ba505448d6210fa3b44578793b9f7b5ff7
Daniel Miller <dm635@cornell.edu>
2016-11-29 12:21:17 -0500

    server skeleton

--- ana63_dm635/src/server.py
+++ ana63_dm635/src/server.py
@@ -1,24 +1,78 @@
+from ast import literal_eval
 import logging as LOG
 import os
+from random import uniform
+from re import match
 from socket import AF_INET, socket, SOCK_STREAM
 import sys
 from threading import Thread, Lock
+from time import sleep
 
-address = 'localhost'
-
-class ClientHandler(Thread):
-    def __init__(self, index, address, port):
+# Listens for messages from clients and servers.
+class ClientServerHandler(Thread):
+    def __init__(self, index, address, connections, log):
         Thread.__init__(self)
+        self.address = address
+        self.connections = connections
         self.index = index
+        self.log = log
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.bind((address, 20000+self.index))
+        self.sock.listen(1)
         LOG.debug('%d: server.ClientHandler()' % self.index)
-    def run():
+    def run(self):
         LOG.debug('%d: server.ClientHandler.run()' % self.index)
+        while True:
+            conn, addr = self.sock.accept()
+            buff = ''
+            valid = True
+            while valid:
+                if '\n' in buff:
+                    (line, rest) = buff.split('\n', 1)
+                    LOG.debug('ClientServerHandler: received \'%s\'' % line)
+                    # Received anti-entropy request from another server.
+                    if match('^\d+:anti-entropy:', line):
+                        line = line.split(':', 2)
+                        sender_id = int(line[0])
+                        vv = literal_eval(line[2])
+                        anti_entropy(self.connections, self.log, self.index, sender_id, vv)
+                else:
+                    try:
+                        buff += conn.recv(1024)
+                    except:
+                        LOG.debug('ClientServerHandler recv failed')
+                        valid = False
+                        conn.close()
+                        break
 
+# send a message to a client
+def sendClient(address, pid, message):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((address, 21000+pid))
+        sock.send(str(message) + '\n')
+        sock.close()
+    except Exception as msg:
+        LOG.debug('server send ERROR: ' + str(msg))
+
+# send a message to a server
+def sendServer(address, pid, message):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((address, 20000+pid))
+        sock.send(str(message) + '\n')
+        sock.close()
+    except Exception as msg:
+        LOG.debug('server send ERROR: ' + str(msg))
+
+# listens for messages from the master
 class MasterHandler(Thread):
-    def __init__(self, index, address, port):
+    def __init__(self, index, address, port, connections, log):
         Thread.__init__(self)
         self.buffer = ''
+        self.connections = connections
         self.index = index
+        self.log = log
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.bind((address, port))
         self.sock.listen(1)
@@ -35,7 +89,7 @@ class MasterHandler(Thread):
                 line = line.split()
                 if 'createConn' == line[0]:
                     s_ids = map(int, line[1:])
-                    # TODO: create connections.
+                    self.connections |= set(s_ids)
                 elif 'breakConn' == line[0]:
                     s_ids = map(int, line[1:])
                     # TODO: break connections.
@@ -47,40 +101,47 @@ class MasterHandler(Thread):
                     pass
             else:
                 try:
-                    data = self.conn.recv(1024)
-                    self.buffer += data
+                    self.buffer += self.conn.recv(1024)
                 except:
                     self.valid = False
                     self.conn.close()
                     break
 
-class ServerHandler(Thread):
-    def __init__(self, index, address, port):
-        Thread.__init__(self)
-        self.index = index
-        LOG.debug('%d: server.ServerHandler()' % self.index)
-    def run():
-        LOG.debug('%d: server.ServerHandler.run()' % self.index)
-        # TODO: do something.
+    def send(self, message):
+        self.conn.send(str(message) + '\n')
+
+# Conducts anti-entropy with process pid.
+def anti_entropy(connections, log, pid, s_id, vv):
+    LOG.debug('%d:server.anti_entropy(%d, %s)' % (pid, s_id, vv))
+    connections.add(s_id)
+
+def V(log):
+    return {-1: 'TODO'}
 
 def main():
-    global address
+    address = 'localhost'
+    connections = set()
+    log = []
 
-    pid = int(sys.argv[1])
+    index = int(sys.argv[1])
     port = int(sys.argv[2])
 
-    LOG.basicConfig(filename='LOG/%d.log' % pid, level=LOG.DEBUG)
-    LOG.debug('%d: server.main()' % pid)
+    LOG.basicConfig(filename='LOG/%d.log' % index, level=LOG.DEBUG)
+    LOG.debug('%d: server.main()' % index)
 
-    chandler = ClientHandler(pid, address, port)
-    shandler = ServerHandler(pid, address, port)
-    mhandler = MasterHandler(pid, address, port)
-    chandler.start()
-    shandler.start()
+    cshandler = ClientServerHandler(index, address, connections, log)
+    mhandler = MasterHandler(index, address, port, connections, log)
+    cshandler.start()
     mhandler.start()
+    LOG.debug('%d: server.main: beginning while loop' % index)
 
-    LOG.debug('%d: server.main ended' % pid)
-
+    while True:
+        # Initiate anti-entropy with all connections
+        message = '%d:anti-entropy:%s' % (index, V(log))
+        for pid in connections:
+            sendServer(address, pid, message)
+        # Wait a bit before conducting anti-entropy again.
+        sleep(uniform(.1,.4))
 
 if __name__ == '__main__':
   main()
