8656df4abb54a21b3eb9f71d182c569a501e62dc
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2016-12-02 10:40:41 -0500

    client master comms

--- ana63_dm635/src/client.py
+++ ana63_dm635/src/client.py
@@ -1,9 +1,13 @@
 import logging as LOG
 from socket import AF_INET, socket, SOCK_STREAM
 import sys
+import serialization
 from threading import Thread, Lock
 
+root_port21k = 21000
 address = 'localhost'
+mHandler = None
+client_counter = 0
 
 class MasterHandler(Thread):
     def __init__(self, index, address, port):
@@ -28,15 +32,37 @@ class MasterHandler(Thread):
                     songName = line[1]
                     URL = line[2]
                     s_id = int(line[3])
-                    # TODO: add the song
+                    ## Send add song to the server
+                    
+                    # increment the seq_no
+                    client_counter++
+                    # get msg payload
+                    msg = client_add(self.index, client_counter, songName, URL)
+                    # send msg to server
+                    send(s_id, msg)
+
                 elif 'delete' == line[0]:
                     songName = line[1]
                     s_id = int(line[2])
-                    # TODO: delete the song
+                    ## Send delete song to the server
+                    
+                    # increment the seq_no
+                    client_counter++
+                    # get msg payload
+                    msg = client_delete(self.index, client_counter, songName)
+                    # send msg to server
+                    send(s_id, msg)
+
                 elif 'get' == line[0]:
                     songName = line[1]
                     s_id = int(line[2])
-                    # TODO: get the URL
+                    ## Send get song to the server
+                    
+                    # get msg payload
+                    msg = client_read(self.index, client_counter, songName)
+                    # send msg to server
+                    send(s_id, msg)
+
             else:
                 try:
                     data = self.conn.recv(1024)
@@ -46,17 +72,45 @@ class MasterHandler(Thread):
                     self.conn.close()
                     break
 
+    def send(self, s):
+        self.conn.send(str(s) + '\n')
+
+    def close(self):
+        try:
+            self.sock.close()
+        except:
+            pass
+
+def send(pid, msg):
+    global mHandler, root_port21k
+    if pid is -1:
+        mHandler.send(msg)
+        return
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((address, root_port21k + pid))
+        sock.send(str(msg) + '\n')
+        sock.close()
+    except:
+        LOG.debug('SOCKET: ERROR ' + str(msg))
+
 class ServerHandler(Thread):
     def __init__(self, index, address, port):
         Thread.__init__(self)
+        self.buffer = ''
         self.index = index
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.bind((address, port))
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
+        self.valid = True
         LOG.debug('%d: client.ServerHandler()' % self.index)
     def run():
         LOG.debug('%d: client.ServerHandler.run()' % self.index)
         # TODO: do something
 
 def main():
-    global address
+    global address, mHandler, root_port21k
 
     pid = int(sys.argv[1])
     port = int(sys.argv[2])
@@ -64,7 +118,7 @@ def main():
     LOG.basicConfig(filename='LOG/%d.log' % pid, level=LOG.DEBUG)
     LOG.debug('%d: client.main()' % pid)
 
-    shandler = ServerHandler(pid, address, port)
+    shandler = ServerHandler(pid, address, root_port21k + pid)
     mhandler = MasterHandler(pid, address, port)
     shandler.start()
     mhandler.start()
