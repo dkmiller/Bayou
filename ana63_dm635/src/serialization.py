ce4f25619b01256cc4265ae2670ed81c93307c4f
Daniel Miller <dm635@cornell.edu>
2016-12-01 22:12:12 -0500

    more serialization

--- ana63_dm635/src/serialization.py
+++ ana63_dm635/src/serialization.py
@@ -11,6 +11,11 @@ def client_delete(c_id, seq_no, song_name):
 def client_read(c_id, seq_no, song_name):
     return client_message(c_id, seq_no, 'get', song_name, None)
 
+# Client should call this to deserialize a message from a server. 
+class ClientDeserialize:
+    def __init__(self, message):
+        pass
+
 # Client should not call this!
 def client_message(c_id, seq_no, write_type, song_name, url):
     message = '%s:%d:%d:%s:%s' % ('client', c_id, seq_no, write_type, song_name)
@@ -18,15 +23,37 @@ def client_message(c_id, seq_no, write_type, song_name, url):
         message += ':%s' % url
     return message
 
-def server_deserialize(message):
-    sender_type, rest = message.split(':',1)
-    if sender_type == 'client':
-        c_id, seq_no, kind, rest = rest.split(':',3)
-        song_name = url = None
-        if kind in ['delete', 'get']:
-            song_name = rest
-        elif kind in ['add']:
-            song_name, url = rest.split(':', 1)
-        return ('client', c_id, seq_no, kind, song_name, url)
-    elif sender_type == 'server':
-        pass
+
+## SERVER CODE.
+
+# Server should call this to serialize a 'connect to me' message.
+def server_connect():
+    pass
+
+def server_disconnect():
+    pass
+
+def server_elect():
+    pass
+
+def server_log():
+    pass
+
+def server_serialize(s_id, s_index, **keywords):
+    pass
+
+# Call this to get an object representing the deserialized state of a message. 
+class ServerDeserialize:
+    def __init__(self, message):
+        self.sender_type, rest = message.split(':',1)
+        if self.sender_type == 'client':
+            self.client_id, self.sequence_number, self.action_type, rest = rest.split(':',3)
+            self.client_id = int(self.client_id)
+            self.sequence_number = int(self.sequence_number)
+            if self.action_type in ['delete', 'get']:
+                self.song_name = rest
+            elif self.action_type in ['add']:
+                self.song_name, self.URL = rest.split(':',1)
+        elif self.sender_type == 'server':
+            pass
+
