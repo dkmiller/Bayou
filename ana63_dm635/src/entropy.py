7f46935a1cc29d4b1e2078a5e12320955f8ec179
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2016-12-03 11:01:37 -0500

    entropy bug with redundant data in ten_log and com_log fixed

--- ana63_dm635/src/entropy.py
+++ ana63_dm635/src/entropy.py
@@ -3,6 +3,8 @@ def anti_entropy(self_log_com, self_log_ten, new_log_com, new_log_ten, vv, new_v
     # Update committed log.
     for log_entry in new_log_com:
         if log_entry not in self_log_com:
+            if log_entry in self_log_ten:
+                self_log_ten.remove(log_entry)
             self_log_com.append(log_entry)
 
     # Update tentative log.
